f0395729ffff449b9346abf5267639c63e2fd6b9
AntonDiachenko <99092120+AntonDiachenko@users.noreply.github.com>
2022-09-29 16:44:06 -0400

    day01-write-read-to-file

--- Day01TextFile/Program.cs
+++ Day01TextFile/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,39 @@ namespace Day01TextFile
     {
         static void Main(string[] args)
         {
+
+            Console.Write("What is your name?");
+            string name = Console.ReadLine();
+
+            using (StreamWriter writer = new StreamWriter(@"C:\Users\anton\Desktop\send\data.txt"))
+            {
+                for (int i = 0; i < 3; i++) { 
+                    writer.WriteLine(name);
+                }
+            }
+
+            /*
+            // Read a file  
+            string readText = File.ReadAllText(@"C:\Users\anton\Desktop\send\data.txt");
+            Console.WriteLine(readText);
+            */
+
+            FileStream fs = new FileStream("C:\\Users\\anton\\Desktop\\send\\data.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            Console.WriteLine("The lines from file are:");
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            string str = sr.ReadLine();
+            while (str != null)
+            {
+                Console.WriteLine(str);
+                str = sr.ReadLine();
+            }
+            Console.ReadLine();
+            sr.Close();
+            fs.Close();
+
+
+
         }
     }
 }
