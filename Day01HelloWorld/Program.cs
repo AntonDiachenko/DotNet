fe0618a8fde3ac5b29752cc99c8b2465c6736f60
AntonDiachenko <99092120+AntonDiachenko@users.noreply.github.com>
2022-09-29 10:38:32 -0400

    01

---
+++ Day01HelloWorld/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Day01HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("What is your name?");
+                string name = Console.ReadLine();
+                Console.Write("What is your age?");
+                string ageStr = Console.ReadLine();
+                if (!int.TryParse(ageStr, out int age))
+                {
+                    Console.WriteLine("Invalid input");
+                    return;
+                }
+                Console.WriteLine($"Hello {name}! You are {age} y/o");
+
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+
+
+            /*
+             *  Console.Write("What is your name?");
+                string name = Console.ReadLine();
+                Console.Write("What is your age?");
+                try
+                {
+                    string ageStr = Console.ReadLine();
+                    int age = int.Parse(ageStr);
+
+                    Console.WriteLine($"Hello {name}! You are {age} y/o");
+                }catch (FormatException ex)
+                {
+                    Console.WriteLine("Invalid input");
+                }
+
+                Console.ReadKey();
+             */
+
+        }
+
+
+
+    }
+}
