85ec987b11d50bb8746baf7ae09297b2e0956755
AntonDiachenko <99092120+AntonDiachenko@users.noreply.github.com>
2022-10-01 22:36:10 -0400

    DAY02

--- Day01TextFile/Program.cs
+++ Day01TextFile/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Day01TextFile
 {
@@ -11,37 +12,79 @@ namespace Day01TextFile
     {
         static void Main(string[] args)
         {
-
-            Console.Write("What is your name?");
-            string name = Console.ReadLine();
-
-            using (StreamWriter writer = new StreamWriter(@"C:\Users\anton\Desktop\send\data.txt"))
+            try
             {
-                for (int i = 0; i < 3; i++) { 
-                    writer.WriteLine(name);
+
+
+                const string filePath = @"..\..\data.txt";
+
+                Console.Write("Wrat is your name?");
+                string name = Console.ReadLine();
+
+                try
+                {
+                    { // write an array of strings
+                        string[] namesArray = { name, name, name };
+                        File.WriteAllLines(filePath, namesArray);
+                    }
+
+                    {
+                        string fileContents = $"{name}\n{name}\n{name}\n";
+                        File.WriteAllText(filePath, fileContents);
+                    }
+                    // NOTE: using Streamwriter is not recommended for small amount of data
+
+                }
+                catch (SystemException ex)
+                {
+                    Console.WriteLine("Error writing to file: " + ex.Message);
+                }
+
+
+
+                /*
+                try 
+                {
+                    using (sw = new StreamWriter(filePath))
+                    {
+                        sw.WriteLine(name);
+                        sw.WriteLine(name);
+                        sw.WriteLine(name);
+                        // Closes file automatically when leaving this block because 'using' is used.
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+                finally 
+                {
+                    Console.WriteLine("Executing finally block.");
+                    Console.ReadKey();
+                }
+                */
+
+                // Reading from file
+                try
+                {
+                    string[] linesArray = File.ReadAllLines(filePath);
+                    foreach (string line in linesArray)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                }
+                catch (SystemException ex)
+                {
+                    Console.WriteLine("Error writing to file: " + ex.Message);
                 }
             }
-
-            /*
-            // Read a file  
-            string readText = File.ReadAllText(@"C:\Users\anton\Desktop\send\data.txt");
-            Console.WriteLine(readText);
-            */
-
-            FileStream fs = new FileStream("C:\\Users\\anton\\Desktop\\send\\data.txt", FileMode.Open, FileAccess.Read);
-            StreamReader sr = new StreamReader(fs);
-            Console.WriteLine("The lines from file are:");
-            sr.BaseStream.Seek(0, SeekOrigin.Begin);
-            string str = sr.ReadLine();
-            while (str != null)
-            {
-                Console.WriteLine(str);
-                str = sr.ReadLine();
+            finally 
+            { 
+                Console.WriteLine("Press any key to finish");
+                Console.ReadKey();
+            
             }
-            Console.ReadLine();
-            sr.Close();
-            fs.Close();
-
 
 
         }
