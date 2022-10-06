97a80eac87e5fe8bff2c4a85ac7dcd46a6c122a8
AntonDiachenko <99092120+AntonDiachenko@users.noreply.github.com>
2022-10-05 21:57:38 -0400

    day04_p1

---
+++ Day02_TempConv/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Day02_TempConv
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TbxInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            double inValue = Double.Parse(TbxInput.Text);
+            if (RbInputC.IsChecked == true && RbOutputC.IsChecked == true)
+            {
+                TbxOutput.Text = $"{inValue}";
+            } 
+            else if (RbInputC.IsChecked == true && RbOutputF.IsChecked == true)
+            {
+                double result = Math.Round((inValue * 1.8 + 32), 2);
+                TbxOutput.Text = $"{result}";
+            }
+            else if (RbInputC.IsChecked == true && RbOutputK.IsChecked == true)
+            {
+                double result = Math.Round((inValue + 273.15), 2);
+                TbxOutput.Text = $"{result}";
+            }
+            else if (RbInputF.IsChecked == true && RbOutputC.IsChecked == true)
+            {
+                double result = Math.Round(((inValue - 32) / 1.8), 2);
+                TbxOutput.Text = $"{result}";
+            }
+            else if (RbInputF.IsChecked == true && RbOutputF.IsChecked == true)
+            {
+                TbxOutput.Text = $"{inValue}";
+            }
+            else if (RbInputF.IsChecked == true && RbOutputK.IsChecked == true)
+            {
+                double result = Math.Round((((inValue - 32) * 5) / 9 +273.15), 2);
+                TbxOutput.Text = $"{result}";
+            }
+            else if (RbInputK.IsChecked == true && RbOutputC.IsChecked == true)
+            {
+                double result = Math.Round((inValue - 273.15), 2);
+                TbxOutput.Text = $"{result}";
+            }
+            else if (RbInputK.IsChecked == true && RbOutputF.IsChecked == true)
+            {
+                double result = Math.Round((((inValue - 273.15)*9)/5 +32), 2);
+                TbxOutput.Text = $"{result}";
+            }
+            else if (RbInputK.IsChecked == true && RbOutputK.IsChecked == true)
+            {
+                TbxOutput.Text = $"{inValue}";
+            }
+            else
+            {
+                MessageBox.Show("Make input and output scale selection first", "Input error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+
+
+        }
+
+    }
+}
---
+++ Day4ListGridViewPeople/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Day4ListGridViewPeople
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<Person> peopleList = new List<Person>();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LvPeople.ItemsSource = peopleList;
+        }
+
+        private void BtnAddPerson_Click(object sender, RoutedEventArgs e)
+        {
+            if(!ArePersonInputsValid()) return;
+            string name = TbxName.Text;
+            int.TryParse(TbxAge.Text, out int age); // ok no need to validate again
+            peopleList.Add(new Person(name, age));
+            LvPeople.Items.Refresh();  // tell ListView data has changed
+            ResetField();
+        }
+
+        private void BtnDeletePerson_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnUpdatePerson_Click(object sender, RoutedEventArgs e)
+        {
+            Person currSelPer = LvPeople.SelectedItem as Person;
+            if (currSelPer == null) return;
+            if (!ArePersonInputsValid()) return;
+            string name = TbxName.Text;
+            int.TryParse(TbxAge.Text, out int age); 
+            currSelPer.Name = name;
+            currSelPer.Age = age;
+            LvPeople.Items.Refresh();  // tell ListView data has changed
+            ResetField();
+        }
+
+
+        private bool ArePersonInputsValid()
+        {
+            string name = TbxName.Text;
+            if (!Person.IsNameValid(name, out string errorName))
+            {
+                MessageBox.Show(this, errorName, "Input error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            string strAge = TbxAge.Text;
+            if(!Person.IsAgeValid(strAge, out string errorAge))
+            {
+                MessageBox.Show(this, errorAge, "Input error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            return true;
+        }
+
+
+        private void ResetField() 
+        {
+            TbxName.Text = "";
+            TbxAge.Text = "";
+        }
+
+        private void LvPeople_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Person currSelPer = LvPeople.SelectedItem as Person;
+            if (currSelPer == null)
+            {
+                ResetField();
+            }
+            else 
+            {
+                TbxName.Text = currSelPer.Name;
+                TbxAge.Text = currSelPer.Age.ToString();
+            }
+        }
+
+        private void MiExport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
